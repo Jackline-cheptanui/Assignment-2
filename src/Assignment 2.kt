8697433a56857a3503c1f3717df9996862610f4b
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-02-28 17:53:06 +0300

    second

---
+++ src/Assignment 2.kt
@@ -0,0 +1,72 @@
+
+fun main() {
+    var name = "hello Jackline"
+    println(name)
+    var modulus = modulus(43, 20)
+    println(modulus)
+    var sum =addition(20,30,40,10)
+    println(sum)
+    interestingFact()
+}
+fun modulus(num1:Int,num2:Int):Int{
+    var modulus=num1%num2
+    return(modulus)
+
+}
+
+fun addition(a:Int,b:Int,c:Int,d:Int):Int {
+    var sum=a+b+c+d
+    return(sum)
+
+
+}
+
+fun interestingFact() {
+    val interestingFact=("curiosity")
+    println(interestingFact)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
